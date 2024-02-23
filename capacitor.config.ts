0a18d472d69475bbe4a5704fb36ef4ca5781317f
ffrhn07@gmail.com <ffrhn07@gmail.com>
2024-02-23 10:52:18 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bookish-octo-computing-machine',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
